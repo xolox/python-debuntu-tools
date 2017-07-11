cc0e08230cde3271c8cd595bdb08250c368086eb
Peter Odding <peter@peterodding.com>
2017-07-11 12:22:43 +0200

    Bump copyright

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # Debian and Ubuntu system administration tools.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: June 15, 2016
+# Last Change: July 11, 2017
 # URL: https://debuntu-tools.readthedocs.io
 
 """Sphinx documentation configuration for the `debuntu-tools` project."""
@@ -34,7 +34,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'debuntu-tools'
-copyright = u'2016, Peter Odding'
+copyright = u'2017, Peter Odding'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
